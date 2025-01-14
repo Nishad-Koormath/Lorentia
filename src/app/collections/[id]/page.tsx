b3080ebee5f520b21650fdbf8bbe29375430d395
nishad <nishadkoormath@gmail.com>
2025-01-14 13:28:57 +0530

    updated styles

--- src/app/collections/[id]/page.tsx
+++ src/app/collections/[id]/page.tsx
@@ -15,33 +15,7 @@ interface DetailPageProps {
 }
 
 const DetailPage = async ({ params }: DetailPageProps) => {
-  return <ClientProductDetails params={ await params } />;
+  return <ClientProductDetails params={await params} />;
 };
 
 export default DetailPage;
-
-
-
-
-
-
-// import React from "react";
-// import { products } from "@/data/product";
-// import "./styles.css";
-// import { Props } from "@/app/types/types";
-// import ClientProductDetails from "../../components/ClientProductDetails";
-
-// export async function generateMetadata({ params }: Props) {
-//   const product_id = parseInt((await params).productId, 10);
-//   const product = products.find((prod) => prod.id == product_id);
-//   return product ? product.name : "Lorentia | product details";
-// }
-// interface DetailPageProps {
-//   params: Promise<{ productId: string }>
-// }
-
-// const DetailPage =  ({ params }: DetailPageProps) => {
-//   return <ClientProductDetails params={params} />;
-// };
-
-// export default DetailPage;
